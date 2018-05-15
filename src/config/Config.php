b99b406bacfefdfb1a86bd8800d99356a343bd55
Sergio Bernal Guillot <sergioguillot@gmail.com>
2018-05-15 17:05:41 +0200

    Parámetros de entorno

---
+++ src/config/Config.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Daw\config;
+
+class Config
+{
+    public const DB_HOST = "";
+    public const DB_PORT = "";
+    public const DB_USERNAME = "";
+    public const DB_PASSWD = "";
+    public const DB_DB = "";
+}
+
+?>
--- src/models/Db.php
+++ src/models/Db.php
@@ -1,15 +1,53 @@
 <?php
+
 namespace Daw\models;
-/**
- * Plantilla vacia de Db
- */
+
+use mysqli;
+use Daw\config\Config;
+
 class Db
 {
+    private $server = "";
+    private $port = "";
+    private $username = "";
+    private $passwd = "";
+    private $db = "";
 
-  function __construct()
-  {
-  }
+    private $connector = "";
 
+    public function __construct()
+    {
+        $this->server = (isset($_ENV["DB_HOST"]) ? $_ENV["DB_HOST"] : Config::DB_HOST);
+        $this->port = (isset($_ENV["DB_PORT"]) ? $_ENV["DB_PORT"] : Config::DB_PORT);
+        $this->username = (isset($_ENV["DB_USERNAME"]) ? $_ENV["DB_USERNAME"] : Config::DB_USERNAME);
+        $this->passwd = (isset($_ENV["DB_PASSWD"]) ? $_ENV["DB_PASSWD"] : Config::DB_PASSWD);
+        $this->db = (isset($_ENV["DB_DB"]) ? $_ENV["DB_DB"] : Config::DB_DB);
+
+        $this->connect();
+    }
+
+    public function connect()
+    {
+        $mysqli = new mysqli(
+            $this->server,
+            $this->username,
+            $this->passwd,
+            $this->db,
+            $this->port
+        );
+
+        if ($mysqli->connect_errno) {
+            $this->connector = false;
+        } else {
+            $this->connector = $mysqli;
+        }
+    }
+
+    /**
+     * Get the value of connector
+     */
+    public function getConnector()
+    {
+        return $this->connector;
+    }
 }
-
-?>
