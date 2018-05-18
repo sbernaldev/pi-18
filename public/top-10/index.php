c1724925b6b6261ce00e9a6e6eb8272bb356e729
Alejandro Artes Soriano <alarso@hotmail.com>
2018-05-18 10:51:45 +0200

    Crear archivo links css

--- public/top-10/index.php
+++ public/top-10/index.php
@@ -11,8 +11,7 @@
     <title>QUOOTER</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-
+    <?php require "../linkscss.php"; ?>
     <!-- Custom styles for this template -->
     <!-- <link href="custom.css" rel="stylesheet"> -->
   </head>
