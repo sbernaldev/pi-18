582bd2dc346139eda8b170a03c9a9c9ef644bef6
Alejandro Artes Soriano <alarso@hotmail.com>
2018-05-18 11:23:20 +0200

    Body
    Todavia no esta terminado, he hecho hasta los primeros campos

--- public/top-10/body.php
+++ public/top-10/body.php
@@ -1,64 +1,28 @@
-<div class="starter-template">
-    <h1>Bootstrap starter template</h1>
-</div>
-<div class="container">
+<hr class="">
+<div class="container target">
     <div class="row">
-    <blockquote class="quote-box ml-5">
-      <p class="quotation-mark ml-2">
-        “
-      </p>
-      <p class="quote-text ml-2">
-        Don't believe anything that you read on the internet, it may be fake. 
-      </p>
-      <hr>
-      <div class="blog-post-actions">
-        <div class="row">
-            <p class="blog-post-bottom pull-left ml-5">
-            Abraham Lincoln1
-            </p>
-            <p class="blog-post-bottom pull-right  ml-5">
-            <span class="badge quote-badge">896❤</span>  
-            </p>
+        <div class="col-sm-10 m-4" >
+             <h1 class="">Starfox221</h1>
+         
+          <button type="button" class="btn btn-success">Book me!</button>  <button type="button" class="btn btn-info">Send me a message</button>
+<br>
         </div>
-      </div>
-    </blockquote>
-    <blockquote class="quote-box ml-4">
-      <p class="quotation-mark ml-2">
-        “
-      </p>
-      <p class="quote-text ml-2">
-        Don't believe anything that you read on the internet, it may be fake. 
-      </p>
-      <hr>
-      <div class="blog-post-actions">
-        <div class="row">
-            <p class="blog-post-bottom pull-left ml-5">
-            Abraham Lincoln2
-            </p>
-            <p class="blog-post-bottom pull-right  ml-5">
-            <span class="badge quote-badge">896❤</span>  
-            </p>
-        </div>
-      </div>
-    </blockquote>
-    <blockquote class="quote-box ml-4">
-      <p class="quotation-mark ml-2">
-        “
-      </p>
-      <p class="quote-text ml-2">
-        Don't believe anything that you read on the internet, it may be fake. 
-      </p>
-      <hr>
-      <div class="blog-post-actions">
-        <div class="row">
-            <p class="blog-post-bottom pull-left ml-5">
-            Abraham Lincoln2
-            </p>
-            <p class="blog-post-bottom pull-right  ml-5">
-            <span class="badge quote-badge">896❤</span>  
-            </p>
-        </div>
-      </div>
-    </blockquote>
-</div>
+      <div class="col-sm-2"><a href="/users" class="pull-right"><img title="profile image" class="img-circle img-responsive" src="http://www.rlsandbox.com/img/profile.jpg"></a>
 
+        </div>
+    </div>
+  <br>
+    <div class="row">
+        <div class="col-sm-3">
+            <!--left col-->
+            <ul class="list-group">
+                <li class="list-group-item text-muted" contenteditable="false">Profile</li>
+                <li class="list-group-item text-right"><span class="pull-left"><strong class="">Joined</strong></span> 2.13.2014</li>
+                <li class="list-group-item text-right"><span class="pull-left"><strong class="">Last seen</strong></span> Yesterday</li>
+                    <li class="list-group-item text-right"><span class="pull-left"><strong class="">Real name</strong></span> Joseph
+                        Doe</li>
+              <li class="list-group-item text-right"><span class="pull-left"><strong class="">Role: </strong></span> Pet Sitter
+               
+                      </li>
+            </ul>
+</div>
