93b6fda735a031da82a88c32a30d18edc3601a9c
Alejandro Artes Soriano <alarso@hotmail.com>
2018-05-18 12:44:05 +0200

    Acabado perfil+css

--- public/top-10/body.php
+++ public/top-10/body.php
@@ -1,28 +1,44 @@
-<hr class="">
-<div class="container target">
-    <div class="row">
-        <div class="col-sm-10 m-4" >
-             <h1 class="">Starfox221</h1>
-         
-          <button type="button" class="btn btn-success">Book me!</button>  <button type="button" class="btn btn-info">Send me a message</button>
-<br>
-        </div>
-      <div class="col-sm-2"><a href="/users" class="pull-right"><img title="profile image" class="img-circle img-responsive" src="http://www.rlsandbox.com/img/profile.jpg"></a>
 
-        </div>
-    </div>
-  <br>
+<hr class="">
+<div class="container target mt-5">
     <div class="row">
-        <div class="col-sm-3">
-            <!--left col-->
-            <ul class="list-group">
-                <li class="list-group-item text-muted" contenteditable="false">Profile</li>
-                <li class="list-group-item text-right"><span class="pull-left"><strong class="">Joined</strong></span> 2.13.2014</li>
-                <li class="list-group-item text-right"><span class="pull-left"><strong class="">Last seen</strong></span> Yesterday</li>
-                    <li class="list-group-item text-right"><span class="pull-left"><strong class="">Real name</strong></span> Joseph
-                        Doe</li>
-              <li class="list-group-item text-right"><span class="pull-left"><strong class="">Role: </strong></span> Pet Sitter
+            
+            
+            
+            <div class="col-3">
+    <div class="row">
+        <h1 class="m-2">Starfox221</h1>
+      
+  </div>
+  <div class="row">
+        
+      <a href="/users" class="pull-right m-2"><img title="profile image" class="img-circle img-responsive" src="http://www.rlsandbox.com/img/profile.jpg"></a>
+  </div><div class="row">
+        
+      
+  <ul class="list-group m-2 col-12">
+                <li class="list-group-item text-muted" contenteditable="false">Perfil</li>
+                <li class="list-group-item text-right"><span class="pull-left"><strong class="">Día registro</strong></span> 2.13.2014</li>
+                <li class="list-group-item text-right"><span class="pull-left"><strong class="">Nombre</strong></span>Alejandro</li>
+                    <li class="list-group-item text-right"><span class="pull-left"><strong class="">Apellido</strong></span>Artes</li>
+              <li class="list-group-item text-right"><span class="pull-left"><strong class="">Rol</strong></span>Administrador
                
                       </li>
-            </ul>
-</div>
+            </ul></div><br>
+      
+          </div><div class="col-9">
+        <div class="row">
+            
+            
+            
+            <ul class="column my-1">
+            <div class="row">
+            <h1>Escribe cosas sobre ti!</h1>
+            <div class="container">
+                <textarea id="congratulation-text" placeholder="Escribe algo que te identifique!"></textarea>
+              </div>
+              </div>
+              </ul></div>
+            
+    </div></div>
+    </div>
