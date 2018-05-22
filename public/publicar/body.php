f16eb142fbeb396812fe9abc3e1e8b0889bf2126
Alejandro Artes Soriano <alarso@hotmail.com>
2018-05-22 13:09:58 +0200

    Inicio página "Publicar"

---
+++ public/publicar/body.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div>        
+        <br style="clear:both">
+            <div class="form-group col-md-4">                                
+                <label id="messageLabel" for="message">Escribe aqui tu cita!</label>
+                <textarea class="form-control input-sm " type="textarea" id="message" placeholder="Escribe aqui tu cita..." maxlength="140" rows="7"></textarea>                   
+            </div>
+        <br style="clear:both">
+        <div class="form-group col-md-2">
+        <button class="form-control input-sm btn btn-success disabled" id="btnSubmit" name="btnSubmit" type="button" style="height:35px">Enviar</button>    
+    </div>
+</div>
