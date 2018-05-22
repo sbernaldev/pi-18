75a9c3bbf01a488ae04f04e71dee7771d023580b
Halleck_Lenovo\Prueba <halleck.g.florida@gmail.com>
2018-05-22 09:09:22 +0200

    cambio carpeta top10

---
+++ public/top-10/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="css/custom.css">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>TOP 10</title>
+
+    <!-- Bootstrap core CSS -->
+    <?php require "../linkscss.php"; ?>
+    <!-- Custom styles for this template -->
+    <!-- <link href="custom.css" rel="stylesheet"> -->
+  </head>
+
+  <body>
+
+    <?php require "../Menu.php"; ?>
+
+    <main role="main" class="container">
+    <?php require "./body.php"; ?>
+    </main>
+
+    <?php require "../Archivosjs.php"; ?>
+  </body>
+</html>
