07e29873fd23f191cad4c2ff16b9650987a65601
Sergio Bernal Guillot <sergioguillot@gmail.com>
2018-05-22 12:57:21 +0200

    Arreglo rutas

--- public/Menu.php
+++ public/Menu.php
@@ -1,7 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
     <a class="navbar-brand" href="#">
-        <a href="index.php"><img src="public/images/logo.png" width="30" height="30" class="d-inline-block align-top mr-3" alt=""></a>Quooter
-
+        <a href="index.php"><img src="/images/logo.png" width="30" height="30" class="d-inline-block align-top mr-3" alt=""></a>Quooter
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -10,16 +9,16 @@
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul class="navbar-nav mr-auto">
             <li class="nav-item active">
-                <a class="nav-link" href="../index.php">Inicio</a>
+                <a class="nav-link" href="/">Inicio</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">Favoritos</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link disabled" href="#">Top 10</a>
+                <a class="nav-link disabled" href="/top-10">Top 10</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="contacto/index.php">Contacto</a>
+                <a class="nav-link" href="/contacto">Contacto</a>
             </li>
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Mi Perfil</a>
@@ -31,8 +30,8 @@
             </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
-          <a class="btn btn-primary" href="../login.php" role="button">Entrar</a>
-          <a class="btn btn-primary ml-3" href="../Registro.php" role"button">Registro</a>
+          <a class="btn btn-primary" href="/login.php" role="button">Entrar</a>
+          <a class="btn btn-primary ml-3" href="/registro.php" role"button">Registro</a>
 
         </form>
     </div>
