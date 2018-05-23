fa52d5bd7d33dcf85d4ebd994c2e3970f137cfb3
Alejandro Artes Soriano <alarso@hotmail.com>
2018-05-23 12:02:49 +0200

    Dropdown en publicar

--- public/publicar/body.php
+++ public/publicar/body.php
@@ -1,4 +1,5 @@
 <div class="container md-6">
+<div class="row">
         <div>
             <br style="clear:both">
                 <div class="form-group col-md-4">
@@ -7,5 +8,16 @@
             <br style="clear:both">
                 <div class="form-group col-md-2">
                 <button class="form-control input-sm btn btn-success disabled" id="btnSubmit" name="btnSubmit" type="button" style="height:35px">PUBLICAR</button>
-                </div>
-    </div>
+        </div>
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            CATEGORIAS
+            </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Amor</a>
+            <a class="dropdown-item" href="#">Tristeza</a>
+            <a class="dropdown-item" href="#">Vida</a>
+        </div>
+        </div>
+</div>
+</div>
