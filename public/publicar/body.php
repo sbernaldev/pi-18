5299d5ec66738a31ec97484d25831f1bf003fb49
Alejandro Artes Soriano <alarso@hotmail.com>
2018-05-23 11:34:45 +0200

    Cambios en body de "Publicar"

--- public/publicar/body.php
+++ public/publicar/body.php
@@ -1,12 +1,11 @@
 <div class="container md-6">
-        <div>        
+        <div>
             <br style="clear:both">
-                <div class="form-group col-md-4">                                
-                    <label id="messageLabel" for="message">Escribe aqui tu cita!</label>
-                    <textarea class="form-control input-sm " type="textarea" id="message" placeholder="Escribe aqui tu cita..." maxlength="140" rows="7"></textarea>                   
+                <div class="form-group col-md-4">
+                    <textarea class="form-control input-sm " type="textarea" id="message" placeholder="Escribe aqui tu cita..." maxlength="140" rows="10"></textarea>
                 </div>
             <br style="clear:both">
                 <div class="form-group col-md-2">
-                <button class="form-control input-sm btn btn-success disabled" id="btnSubmit" name="btnSubmit" type="button" style="height:35px">Enviar</button>    
+                <button class="form-control input-sm btn btn-success disabled" id="btnSubmit" name="btnSubmit" type="button" style="height:35px">PUBLICAR</button>
                 </div>
     </div>
