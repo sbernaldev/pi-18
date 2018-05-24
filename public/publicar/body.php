d8ac2860867b61d1fc6870a0967de6dd873a0268
Alejandro Artes Soriano <alarso@hotmail.com>
2018-05-24 08:54:29 +0200

    Añadimos dropdown a la pagina "Publicar" para seleccionar la categoria de la cita

--- public/publicar/body.php
+++ public/publicar/body.php
@@ -1,23 +1,19 @@
-<div class="container md-6">
-<div class="row">
-        <div>
-            <br style="clear:both">
-                <div class="form-group col-md-4">
-                    <textarea class="form-control input-sm " type="textarea" id="message" placeholder="Escribe aqui tu cita..." maxlength="140" rows="10"></textarea>
-                </div>
-            <br style="clear:both">
-                <div class="form-group col-md-2">
-                <button class="form-control input-sm btn btn-success disabled" id="btnSubmit" name="btnSubmit" type="button" style="height:35px">PUBLICAR</button>
-        </div>
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            CATEGORIAS
-            </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Amor</a>
-            <a class="dropdown-item" href="#">Tristeza</a>
-            <a class="dropdown-item" href="#">Vida</a>
-        </div>
-        </div>
-</div>
+<div class="container md-12">
+    <div class="row">
+            <div class="form-group col-md-10 mt-3">
+                <textarea class="form-control input-sm " type="textarea" id="message" placeholder="Escribe aqui tu cita..." maxlength="140" rows="10"></textarea>
+            </div>
+            <div class="form-group col-md-6">
+                <select class="form-control" id="Selecciona tu categoria!">
+                    <option>Selecciona tu categoria</option>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                </select>
+            </div>
+            <div class="form-group col-md-4">
+            <button class="form-control input-sm btn btn-success disabled" id="btnSubmit" name="btnSubmit" type="button" style="height:35px">PUBLICAR</button>
+            </div>
+    </div> 
 </div>
