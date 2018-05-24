b87a2c78e7fb7a6e5ebfcbf564a273c06833f0e9
Halleck_Lenovo\Prueba <halleck.g.florida@gmail.com>
2018-05-24 12:15:49 +0200

    Añadido favorito a inicio

--- public/body_index.php
+++ public/body_index.php
@@ -1,56 +1,120 @@
-<br><br>
-<div class="container mt-5">
+
+<div class="container mt-4">
   <div class="row">
       <div class="col-sm ml-4">
           <div class="">
+              <span class="">
+              <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a></span>
               <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                   <p class="mb-0  ">2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                   <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
               </blockquote>
+              <div class="col-sm text-right">
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE LIKES DE LA CITA -->
+                  <p class="badge badge-light">likes
+                      <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
+                  <span class="badge badge-light">dislikes
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+              </div>
           </div>
       </div>
       <div class="col-sm">
           <div class="">
+              <span class="">
+              <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a></span>
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
             </blockquote>
+              <div class="col-sm text-right">
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE LIKES DE LA CITA -->
+                  <p class="badge badge-light">likes
+                      <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
+                  <span class="badge badge-light">dislikes
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+              </div>
           </div>
       </div>
       <div class="col-sm mr-4">
           <div class="">
+              <span class="">
+              <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a></span>
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
             </blockquote>
+              <div class="col-sm text-right">
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE LIKES DE LA CITA -->
+                  <p class="badge badge-light">likes
+                      <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
+                  <span class="badge badge-light">dislikes
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+              </div>
           </div>
       </div>
   </div>
 </div>
-<div class="container mt-5">
+
+
+
+
+<div class="container mt-3">
   <div class="row">
       <div class="col-sm ml-4">
           <div class="">
+              <span class="">
+              <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a></span>
               <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                   <p class="mb-0  ">2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                   <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
               </blockquote>
+              <div class="col-sm text-right">
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE LIKES DE LA CITA -->
+                  <p class="badge badge-light">likes
+                      <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
+                  <span class="badge badge-light">dislikes
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+              </div>
           </div>
       </div>
       <div class="col-sm">
           <div class="">
+              <span class="">
+              <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a></span>
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
             </blockquote>
+              <div class="col-sm text-right">
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE LIKES DE LA CITA -->
+                  <p class="badge badge-light">likes
+                      <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
+                  <span class="badge badge-light">dislikes
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+              </div>
           </div>
       </div>
       <div class="col-sm mr-4">
           <div class="">
+              <span class="">
+              <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a></span>
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
             </blockquote>
+              <div class="col-sm text-right">
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE LIKES DE LA CITA -->
+                  <p class="badge badge-light">likes
+                      <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
+                  <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
+                  <span class="badge badge-light">dislikes
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+              </div>
           </div>
       </div>
   </div>
