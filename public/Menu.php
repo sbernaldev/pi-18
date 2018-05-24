73ada6aa6b06bbfb2c444d6e82aa64d675bddbca
Alejandro Artes Soriano <alarso@hotmail.com>
2018-05-24 11:30:05 +0200

    Subo publicar y meto href en el Menu.php

--- public/Menu.php
+++ public/Menu.php
@@ -13,7 +13,7 @@
                 <a class="nav-link" href="/">Inicio</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Publicar cita</a>
+                <a class="nav-link" href="/publicar">Publicar cita</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/top-10">Top 10</a>
--- public/publicar/body.php
+++ public/publicar/body.php
@@ -1,4 +1,4 @@
-<div class="container md-12">
+<div class="container md-12 mt-5">
     <div class="row">
             <div class="form-group col-md-10 mt-3">
                 <textarea class="form-control input-sm " type="textarea" id="message" placeholder="Escribe aqui tu cita..." maxlength="140" rows="10"></textarea>
@@ -6,14 +6,15 @@
             <div class="form-group col-md-6">
                 <select class="form-control" id="Selecciona tu categoria!">
                     <option>Selecciona tu categoria</option>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
+                    <option>Amor</option>
+                    <option>Amistad</option>
+                    <option>Vida</option>
+                    <option>Filosofica</option>
+                    <option>Positiva</option>
                 </select>
             </div>
             <div class="form-group col-md-4">
             <button class="form-control input-sm btn btn-success disabled" id="btnSubmit" name="btnSubmit" type="button" style="height:35px">PUBLICAR</button>
             </div>
-    </div> 
+    </div>
 </div>
