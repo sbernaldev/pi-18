6636a0e743ce48558b92ed9b447fc80af27de8f6
Halleck_Lenovo\Prueba <halleck.g.florida@gmail.com>
2018-05-24 12:39:52 +0200

    Añadido el enlace añadir a favoritos

--- public/top-10/body.php
+++ public/top-10/body.php
@@ -6,19 +6,26 @@
     <h1 style="font-family:Wide Latin">"BEASTS"</h1>
     <h2 style="font-family:sans-serif" >DE QUOOTER</h2>
 </div>
+<br>
+<br>
 
-<br>
-<br>
+
+
 <!-- PUESTO 1 -->
 <div class="row">
     <div class="col-sm ml-4">
 
     </div>
     <div class="col-sm">
+        <!-- AQUÍ EMPIEZA LA CAJA DE CITAS -->
         <div class="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE LA POSICION EN EL RANKING -->
             <img class= "mb-1" src="../images/ganador01.png" width="33" height="47" class="" alt="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE AGREGAR A FAVORITOS -->
+            <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a>
+            <!-- AQUÍ DEBAJO IRÁ LA CITA -->
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
-                <p class="mb-0  ">2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <p class="mb-0  ">1 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
             </blockquote>
             <div class="col-sm text-right">
@@ -27,9 +34,10 @@
                     <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
                 <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
                 <span class="badge badge-light">dislikes
-                <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
             </div>
         </div>
+        <!-- AQUÍ TERMINA LA CAJA DE CITAS -->
     </div>
     <div class="col-sm mr-4">
 
@@ -37,13 +45,21 @@
 </div>
 
 
+
+
+
 <br>
 <!---- PUESTOS 2-4---->
 
 <div class="row">
     <div class="col-sm ml-4">
+        <!-- AQUÍ EMPIEZA LA CAJA DE CITAS -->
         <div class="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE LA POSICION EN EL RANKING -->
             <img class= "mb-1" src="../images/ganador02.png" width="33" height="47" class="" alt="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE AGREGAR A FAVORITOS -->
+            <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a>
+            <!-- AQUÍ DEBAJO IRÁ LA CITA -->
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">2 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
@@ -54,13 +70,21 @@
                     <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
                 <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
                 <span class="badge badge-light">dislikes
-                <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
             </div>
         </div>
+        <!-- AQUÍ TERMINA LA CAJA DE CITAS -->
     </div>
+
+
     <div class="col-sm">
+        <!-- AQUÍ EMPIEZA LA CAJA DE CITAS -->
         <div class="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE LA POSICION EN EL RANKING -->
             <img class= "mb-1" src="../images/ganador03.png" width="33" height="47" class="" alt="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE AGREGAR A FAVORITOS -->
+            <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a>
+            <!-- AQUÍ DEBAJO IRÁ LA CITA -->
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">3 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
@@ -74,10 +98,18 @@
                <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
             </div>
         </div>
+        <!-- AQUÍ TERMINA LA CAJA DE CITAS -->
     </div>
+
+
     <div class="col-sm mr-4">
+        <!-- AQUÍ EMPIEZA LA CAJA DE CITAS -->
         <div class="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE LA POSICION EN EL RANKING -->
             <img class= "mb-1" src="../images/ganador04.png" width="33" height="47" class="" alt="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE AGREGAR A FAVORITOS -->
+            <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a>
+            <!-- AQUÍ DEBAJO IRÁ LA CITA -->
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">4 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
@@ -88,11 +120,14 @@
                     <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
                 <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
                 <span class="badge badge-light">dislikes
-                <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
             </div>
         </div>
+        <!-- AQUÍ TERMINA LA CAJA DE CITAS -->
     </div>
 </div>
+
+
 
 
 <br>
@@ -100,8 +135,13 @@
 
 <div class="row">
     <div class="col-sm ml-4">
+        <!-- AQUÍ EMPIEZA LA CAJA DE CITAS -->
         <div class="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE LA POSICION EN EL RANKING -->
             <img class= "mb-1" src="../images/ganador05.png" width="33" height="47" class="" alt="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE AGREGAR A FAVORITOS -->
+            <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a>
+            <!-- AQUÍ DEBAJO IRÁ LA CITA -->
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">5 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
@@ -112,13 +152,21 @@
                     <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
                 <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
                 <span class="badge badge-light">dislikes
-                <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
             </div>
         </div>
+        <!-- AQUÍ TERMINA LA CAJA DE CITAS -->
     </div>
+
+
     <div class="col-sm">
+        <!-- AQUÍ EMPIEZA LA CAJA DE CITAS -->
         <div class="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE LA POSICION EN EL RANKING -->
             <img class= "mb-1" src="../images/ganador06.png" width="33" height="47" class="" alt="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE AGREGAR A FAVORITOS -->
+            <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a>
+            <!-- AQUÍ DEBAJO IRÁ LA CITA -->
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">6 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
@@ -132,10 +180,18 @@
                <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
             </div>
         </div>
+        <!-- AQUÍ TERMINA LA CAJA DE CITAS -->
     </div>
+
+
     <div class="col-sm mr-4">
+        <!-- AQUÍ EMPIEZA LA CAJA DE CITAS -->
         <div class="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE LA POSICION EN EL RANKING -->
             <img class= "mb-1" src="../images/ganador07.png" width="33" height="47" class="" alt="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE AGREGAR A FAVORITOS -->
+            <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a>
+            <!-- AQUÍ DEBAJO IRÁ LA CITA -->
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">7 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
@@ -146,19 +202,27 @@
                     <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
                 <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
                 <span class="badge badge-light">dislikes
-                <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
             </div>
         </div>
+        <!-- AQUÍ TERMINA LA CAJA DE CITAS -->
     </div>
 </div>
+
+
 
 <br>
 <!---- PUESTOS 8-10---->
 
 <div class="row">
     <div class="col-sm ml-4">
+        <!-- AQUÍ EMPIEZA LA CAJA DE CITAS -->
         <div class="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE LA POSICION EN EL RANKING -->
             <img class= "mb-1" src="../images/ganador08.png" width="33" height="47" class="" alt="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE AGREGAR A FAVORITOS -->
+            <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a>
+            <!-- AQUÍ DEBAJO IRÁ LA CITA -->
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">8 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
@@ -169,13 +233,21 @@
                     <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
                 <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
                 <span class="badge badge-light">dislikes
-                <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
             </div>
         </div>
+        <!-- AQUÍ TERMINA LA CAJA DE CITAS -->
     </div>
+
+
     <div class="col-sm">
+        <!-- AQUÍ EMPIEZA LA CAJA DE CITAS -->
         <div class="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE LA POSICION EN EL RANKING -->
             <img class= "mb-1" src="../images/ganador09.png" width="33" height="47" class="" alt="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE AGREGAR A FAVORITOS -->
+            <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a>
+            <!-- AQUÍ DEBAJO IRÁ LA CITA -->
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">9 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
@@ -189,10 +261,18 @@
                <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
             </div>
         </div>
+        <!-- AQUÍ TERMINA LA CAJA DE CITAS -->
     </div>
+
+
     <div class="col-sm mr-4">
+        <!-- AQUÍ EMPIEZA LA CAJA DE CITAS -->
         <div class="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE LA POSICION EN EL RANKING -->
             <img class= "mb-1" src="../images/ganador10.png" width="33" height="47" class="" alt="">
+            <!-- AQUÍ DEBAJO IRÁ EL ICONO DE AGREGAR A FAVORITOS -->
+            <a href="" class="fa fa-heart ml-2 mb-1" style="text-decoration:none"></a>
+            <!-- AQUÍ DEBAJO IRÁ LA CITA -->
             <blockquote class="blockquote text-left border border-dark shadow p-3 mb-3 bg-white rounded">
                 <p class="mb-0  ">10 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
                 <footer class="blockquote-footer text-right ml-5">Nombre Usuario <cite title="Source Title">Fecha</cite></footer>
@@ -203,10 +283,10 @@
                     <a  href="" class="fa fa-thumbs-o-up" style="font-size:14px;color:blue;text-decoration:none"></a></p> 
                 <!-- AQUÍ DEBAJO IRÁN LA CANTIDAD DE DISSLIKES DE LA CITA -->
                 <span class="badge badge-light">dislikes
-                <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
+               <a href="" class="fa fa-thumbs-o-down" style="font-size:14px;color:red;text-decoration:none"></a></span>
             </div>
         </div>
+        <!-- AQUÍ TERMINA LA CAJA DE CITAS -->
     </div>
 </div>
-
 <br>
