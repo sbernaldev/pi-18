476b8b22cf52acbfc9d936ebeeaf14818b6caa98
Sergio Bernal Guillot <sergioguillot@gmail.com>
2018-05-26 12:28:43 +0200

    Unifico función peticion en crear usuario

--- public/js/Api/Usuario/Crear.js
+++ public/js/Api/Usuario/Crear.js
@@ -22,117 +22,112 @@ function peticion() {
         'repiteContrasenya': repiteContrasenya       // string
     },
     function(response, status) {
-        responseJSON = JSON.parse(response);
-        console.log(respuesta);
-        respuesta(responseJSON);
+        var responseJSON = JSON.parse(response)
+        // Manejamos cada tipo de respuesta. Un if por cada validación
+        if (responseJSON["validacion"]["correo"] != "valido" ){
+            switch (responseJSON["validacion"]["correo"]) {
+                case "vacio":
+                    alert("El campo correo está vacio");
+                    break;
+                case "invalido":
+                    alert("El campo correo no es válido");
+                    break;
+                case "no_disponible":
+                    alert("El correo electrónico está en uso");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (responseJSON["validacion"]["correoConfirmacion"] != "valido" ){
+            switch (responseJSON["validacion"]["correoConfirmacion"]) {
+                case "vacio":
+                    alert("El campo de confirmar correo está vacio");
+                    break;
+                case "invalido":
+                    alert("El campo de confirmar correo no es válido");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (responseJSON["validacion"]["nom_usuario"] != "valido" ){
+            switch (responseJSON["validacion"]["nom_usuario"]) {
+                case "vacio":
+                    alert("El campo nombre de usuario está vacio");
+                    break;
+                case "invalido":
+                    alert("El campo nombre de usuario no es válido");
+                    break;
+                case "no_disponible":
+                    alert("El nombre de usuario no está disponible");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (responseJSON["validacion"]["nombre"] != "valido" ){
+            switch (responseJSON["validacion"]["nombre"]) {
+                case "vacio":
+                    alert("El campo nombre está vacio");
+                    break;
+                case "invalido":
+                    alert("El campo nombre no es válido");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (responseJSON["validacion"]["apellido"] != "valido" ){
+            switch (responseJSON["validacion"]["apellido"]) {
+                case "vacio":
+                    alert("El campo apellido está vacio");
+                    break;
+                case "invalido":
+                    alert("El campo apellido no es válido");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (responseJSON["validacion"]["contrasenya"] != "valido" ){
+            switch (responseJSON["validacion"]["contrasenya"]) {
+                case "vacio":
+                    alert("El campo contraseña está vacio");
+                    break;
+                case "invalido":
+                    alert("El campo contraseña no es válido");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        if (responseJSON["validacion"]["repiteContrasenya"] != "valido" ){
+            switch (responseJSON["validacion"]["repiteContrasenya"]) {
+                case "vacio":
+                    alert("El campo repite contraseña está vacio");
+                    break;
+                case "invalido":
+                    alert("El campo repite contraseña no es válido");
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // Si la request se ha completado satisfactoriamente aviso al usuario
+        if (responseJSON["registrado"] == true){
+            alert("El usuario ha sido dado de alta correctamente.")
+            return true;
+        } else {
+            alert("El usuario no ha podido darse de alta correctamente.")
+            return false;
+        }
     })
-}
-
-function respuesta(responseJSON) {
-       // Manejamos cada tipo de respuesta. Un if por cada validación
-       if (responseJSON["validacion"]["correo"] != "valido" ){
-        switch (responseJSON["validacion"]["correo"]) {
-            case "vacio":
-                alert("El campo correo está vacio");
-                break;
-            case "invalido":
-                alert("El campo correo no es válido");
-                break;
-            case "no_disponible":
-                alert("El campo correo no está disnponible");
-                break;
-            default:
-                break;
-        }
-    }
-
-    if (responseJSON["validacion"]["correoConfirmacion"] != "valido" ){
-        switch (responseJSON["validacion"]["correoConfirmacion"]) {
-            case "vacio":
-                alert("El campo de confirmar correo está vacio");
-                break;
-            case "invalido":
-                alert("El campo de confirmar correo no es válido");
-                break;
-            default:
-                break;
-        }
-    }
-
-    if (responseJSON["validacion"]["nom_usuario"] != "valido" ){
-        switch (responseJSON["validacion"]["nom_usuario"]) {
-            case "vacio":
-                alert("El campo nombre de usuario está vacio");
-                break;
-            case "invalido":
-                alert("El campo nombre de usuario no es válido");
-                break;
-            case "no_disponible":
-                alert("El nombre de usuario no está disponible");
-                break;
-            default:
-                break;
-        }
-    }
-
-    if (responseJSON["validacion"]["nombre"] != "valido" ){
-        switch (responseJSON["validacion"]["nombre"]) {
-            case "vacio":
-                alert("El campo nombre está vacio");
-                break;
-            case "invalido":
-                alert("El campo nombre no es válido");
-                break;
-            default:
-                break;
-        }
-    }
-
-    if (responseJSON["validacion"]["apellido"] != "valido" ){
-        switch (responseJSON["validacion"]["apellido"]) {
-            case "vacio":
-                alert("El campo apellido está vacio");
-                break;
-            case "invalido":
-                alert("El campo apellido no es válido");
-                break;
-            default:
-                break;
-        }
-    }
-
-    if (responseJSON["validacion"]["contrasenya"] != "valido" ){
-        switch (responseJSON["validacion"]["contrasenya"]) {
-            case "vacio":
-                alert("El campo contraseña está vacio");
-                break;
-            case "invalido":
-                alert("El campo contraseña no es válido");
-                break;
-            default:
-                break;
-        }
-    }
-
-    if (responseJSON["validacion"]["repiteContrasenya"] != "valido" ){
-        switch (responseJSON["validacion"]["repiteContrasenya"]) {
-            case "vacio":
-                alert("El campo repite contraseña está vacio");
-                break;
-            case "invalido":
-                alert("El campo repite contraseña no es válido");
-                break;
-            default:
-                break;
-        }
-    }
-
-    // Si la request se ha completado satisfactoriamente aviso al usuario
-    if (responseJSON["registrado"] == true){
-        alert("El usuario ha sido dado de alta correctamente.")
-        return true;
-    } else {
-        alert("El usuario no ha podido darse de alta correctamente.")
-        return false;
-    }
 }
