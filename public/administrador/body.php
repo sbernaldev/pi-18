0e426d48a62c43098441ad6a36033031e59e7a31
Halleck_Lenovo\Prueba <halleck.g.florida@gmail.com>
2018-05-28 13:41:52 +0200

    editado usuarios administracion

--- public/administrador/body.php
+++ public/administrador/body.php
@@ -10,36 +10,32 @@
               <table id="mytable" class="table table-bordred table-striped">
 
                    <thead>
-
-                   <th><input type="checkbox" id="checkall" /></th>
-                   <th>Nombre</th>
-                    <th>Apellido</th>
-                     <th>Cita</th>
-                     <th>Email</th>
-                     <th>Teléfono</th>
-                      <th>Actualizar</th>
+                       <th><input type="checkbox" id="checkall" /></th>
+                       <th>Nombre</th>
+                       <th>Apellido</th>
+                       <th>Cita</th>
+                       <th>Email</th>
+                       <th>Actualizar</th>
                        <th>Eliminar</th>
                    </thead>
     <tbody>
 
     <tr>
     <td><input type="checkbox" class="checkthis" /></td>
-    <td>Mohsin</td>
-    <td>Irshad</td>
-    <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-    <td>isometric.mohsin@gmail.com</td>
-    <td>+923335586757</td>
+    <td>Marianico</td>
+    <td>El Corto</td>
+    <td>It’s very difficult todo esto.</td>
+    <td>Chorimangui@pp.es</td>
     <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
     <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
     </tr>
 
  <tr>
     <td><input type="checkbox" class="checkthis" /></td>
-    <td>Mohsin</td>
-    <td>Irshad</td>
-    <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-    <td>isometric.mohsin@gmail.com</td>
-    <td>+923335586757</td>
+    <td>Agustín</td>
+    <td>San</td>
+    <td>La medida del amor es amar sin medida.</td>
+    <td>arribadeltodo@heaven.es</td>
     <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
     <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
     </tr>
@@ -47,11 +43,10 @@
 
  <tr>
     <td><input type="checkbox" class="checkthis" /></td>
-    <td>Mohsin</td>
-    <td>Irshad</td>
-    <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-    <td>isometric.mohsin@gmail.com</td>
-    <td>+923335586757</td>
+    <td>Casanova</td>
+    <td></td>
+    <td>No hay nada que un hombre no sea capaz de hacer cuando una mujer le mira.</td>
+    <td>Elmillor@venecia.it</td>
     <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
     <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
     </tr>
@@ -60,11 +55,10 @@
 
  <tr>
     <td><input type="checkbox" class="checkthis" /></td>
-    <td>Mohsin</td>
-    <td>Irshad</td>
-    <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-    <td>isometric.mohsin@gmail.com</td>
-    <td>+923335586757</td>
+    <td>Bob</td>
+    <td>Marley</td>
+    <td>El dinero no puede comprar la vida.</td>
+    <td>MariaMaria@toy.hi</td>
     <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
     <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
     </tr>
@@ -72,11 +66,10 @@
 
  <tr>
     <td><input type="checkbox" class="checkthis" /></td>
-    <td>Mohsin</td>
-    <td>Irshad</td>
-    <td>CB 106/107 Street # 11 Wah Cantt Islamabad Pakistan</td>
-    <td>isometric.mohsin@gmail.com</td>
-    <td>+923335586757</td>
+    <td>Marilyn</td>
+    <td>Monroe</td>
+    <td>Vivir sola es como estar en una fiesta donde nadie te hace caso.</td>
+    <td>chaneln5@whitehouse.com</td>
     <td><p data-placement="top" data-toggle="tooltip" title="Edit"><button class="btn btn-primary btn-xs" data-title="Edit" data-toggle="modal" data-target="#edit" ><span class="glyphicon glyphicon-pencil"></span></button></p></td>
     <td><p data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs" data-title="Delete" data-toggle="modal" data-target="#delete" ><span class="glyphicon glyphicon-trash"></span></button></p></td>
     </tr>
